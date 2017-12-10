558d819e5d5c89307d0852b9fb73b61f5f4d8ddb
Evenedric <evenedric@gmail.com>
2017-12-09 17:31:55 -0800

    Import static plugin classes for static plugins

---
+++ rama/qt/plugin_import.cc
@@ -0,0 +1,18 @@
+// Import static plugin classes for static plugins.
+
+#include <QtPlugin>
+
+Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin)
+
+// Some others that might come in handy later:
+//   Q_IMPORT_PLUGIN(QGifPlugin)
+//   Q_IMPORT_PLUGIN(QICNSPlugin)
+//   Q_IMPORT_PLUGIN(QICOPlugin)
+//   Q_IMPORT_PLUGIN(QJpegPlugin)
+//   Q_IMPORT_PLUGIN(QMacJp2Plugin)
+//   Q_IMPORT_PLUGIN(QTgaPlugin)
+//   Q_IMPORT_PLUGIN(QTiffPlugin)
+//   Q_IMPORT_PLUGIN(QWbmpPlugin)
+//   Q_IMPORT_PLUGIN(QWebpPlugin)
+//   Q_IMPORT_PLUGIN(QCoreWlanEnginePlugin)
+//   Q_IMPORT_PLUGIN(QGenericEnginePlugin)
