9e92c8fc4f8c53d21da89b9d5a87033f2d15a72c
Evenedric <evenedric@gmail.com>
2020-05-25 20:28:51 -0700

    Fixed and moved example model

--- rama/examples/horn_Ez.lua
+++ rama/examples/horn_Ez.lua
@@ -10,12 +10,10 @@ ParameterDivider()
 
 -- Basic parameters.
 F = 100                 -- Feed length
-B = 122                 -- Waveguide B dimension
+B = 122                 -- Waveguide short dimension
 L = Parameter{label='Horn length', min=50, max=800, default=200}
 W = Parameter{label='Horn width',  min=50, max=800, default=200}
 R = Parameter{label='ABC radius',  min=50, max=800, default=300}
-
-local eps = 1e-6        -- @@@ want to remove the need for this
 
 -- Radiation boundary.
 local radius = R + W/2
@@ -25,10 +23,12 @@ rad = rad * Rectangle(-20, -2*radius, 2*radius, 2*radius)
 rad = rad - Rectangle(-50, -0.9*radius, 0, 0.9*radius)
 rad:Offset(F+L, 0)
 
+-- Horn.
 cd = Rectangle(0, -B/2, F, B/2) +
      Shape():AddPoint(F, B/2):AddPoint(F, -B/2)
-            :AddPoint(F+L+eps, -W/2):AddPoint(F+L+eps, W/2) +
-     rad
+            :AddPoint(F+L, -W/2):AddPoint(F+L, W/2) + rad
 
+-- Ports.
 cd:Port(cd:Select(0, 0), 1)
+
 config.cd = cd
